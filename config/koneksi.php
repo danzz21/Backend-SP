b1956fedd669cc4acaded749a7dd6de66a08e7cd
danzz21 <danzzgendut@gmail.com>
2026-05-25 09:47:29 +0700

    koneksi

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,34 +1,13 @@
 <?php
 
-header("Access-Control-Allow-Origin: https://emakh.netlify.app");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 $host = getenv('MYSQLHOST');
 $user = getenv('MYSQLUSER');
 $pass = getenv('MYSQLPASSWORD');
 $db   = getenv('MYSQLDATABASE');
 $port = getenv('MYSQLPORT');
 
-$conn = mysqli_connect(
-    $host,
-    $user,
-    $pass,
-    $db,
-    $port
-);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
-
-echo "Koneksi berhasil";
