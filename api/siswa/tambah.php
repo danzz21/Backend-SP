c1d6cf1b834f5f0715b49000523572e097c7935d
danzz21 <danzzgendut@gmail.com>
2026-05-26 09:41:41 +0700

    tambah

--- api/siswa/tambah.php
+++ api/siswa/tambah.php
@@ -7,31 +7,37 @@ header("Content-Type: application/json");
 
 include "../../config/koneksi.php";
 
-$nama = $_POST['nama'] ?? '';
+$nama  = $_POST['nama'] ?? '';
 $kelas = $_POST['kelas'] ?? '';
 
 if (empty($nama) || empty($kelas)) {
+
     echo json_encode([
         "status" => "error",
         "message" => "Nama dan kelas wajib diisi"
     ]);
+
     exit;
 }
 
-$query = mysqli_query($koneksi, "
+$query = mysqli_query($conn, "
     INSERT INTO siswa (nama_siswa, kelas)
     VALUES ('$nama', '$kelas')
 ");
 
 if ($query) {
+
     echo json_encode([
         "status" => "success",
         "message" => "Data berhasil ditambahkan"
     ]);
+
 } else {
+
     echo json_encode([
         "status" => "error",
-        "message" => mysqli_error($koneksi)
+        "message" => mysqli_error($conn)
     ]);
+
 }
 ?>
