e54e1439f5e788872a331cbf9d216b72f16a74ee
danzz21 <danzzgendut@gmail.com>
2026-05-26 09:32:04 +0700

    ya

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,14 +1,13 @@
 <?php
 
-$host = $_ENV['MYSQLHOST'];
-$user = $_ENV['MYSQLUSER'];
-$pass = $_ENV['MYSQLPASSWORD'];
-$db   = $_ENV['MYSQLDATABASE'];
-$port = $_ENV['MYSQLPORT'];
+$conn = mysqli_connect(
+    getenv("MYSQLHOST"),
+    getenv("MYSQLUSER"),
+    getenv("MYSQLPASSWORD"),
+    getenv("MYSQLDATABASE"),
+    getenv("MYSQLPORT")
+);
 
-$koneksi = mysqli_connect($host, $user, $pass, $db, $port);
-
-if (!$koneksi) {
+if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
-?>
